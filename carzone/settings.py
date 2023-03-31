4e8de3febd9bee1d3ef9a01504e143a510595f66
Vikrant Dekate <vikrant.dekate1203@gmail.com>
2023-04-01 02:22:52 +0530

    test db

--- carzone/settings.py
+++ carzone/settings.py
@@ -91,18 +91,21 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carzone_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-    }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'carzone_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#     }
+# }
 
 
 # DATABASES = {'default': dj_database_url.config(default='postgres://postgres:######@localhost/carzone_db')}
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
 
 
 # Password validation
